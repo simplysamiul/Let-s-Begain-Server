bf8a6fe0f4948794974ab7ef8c99463fd7121bd3
simplysamiul <simplysamiul@gmail.com>
2021-10-31 08:25:05 +0600

    added 2nd time code

--- index.js
+++ index.js
@@ -1,47 +1,51 @@
 const express = require ("express");
 const cors = require ("cors");
 const { MongoClient } = require('mongodb');
+const objectId = require("mongodb").ObjectId;
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
-
-
-// Middleware 
+ 
+ 
+// Middleware
 app.use(cors());
 app.use(express.json());
-
+ 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2pwml.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
+ 
 async function run() {
     try{
         await client.connect();
-
+ 
         const database = client.db("package");
         const packageCollection = database.collection("items");
-
-        // Get Api 
+ 
+        // Get Api
         app.get('/packages', async(req,res)=>{
             const cursor = packageCollection.find({});
             const result = await cursor.toArray();
             res.send(result);
         });
-
+ 
         // Get Specific Api
         app.get('/packages/:id', async(req,res)=>{
-            
-        })
+            const id = req.params.id;
+            const query = {_id: objectId(id)};
+            const result = await packageCollection.findOne(query);
+            res.send(result);
+        });
     }
     finally{
         // await client.close();
     }
 }
 run().catch(console.dir);
-
+ 
 app.get('/', (req,res)=>{
     res.send("Server Started");
 })
-
+ 
 app.listen(port, (req,res)=>{
     console.log(`Port listing at ${port}`);
 })
